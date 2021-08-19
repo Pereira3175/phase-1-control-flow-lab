a36a5648e2c2e2e2edbe36cd82243e18019203bc
Luis <pereira1214@gmail.com>
2021-08-19 02:05:10 -0400

    commit tree 263d8a564845da0012323184515c5fc3b59ff581

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber) {
   // Write your code here!
+  if (someNumber <= 400) {
+    return 'This one is on me!' ;
+  }
+   else if (someNumber >= 2000 &&  someNumber<= 2500) {
+     return 'I will gladly take your thirty bucks.'
+   }
+    else if (someNumber > 2500) {
+      return 'No can do.'
+    }
+  }
+  scuberGreetingForFeet(2501)
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default :
+      return 'Bye.'
+  }
 }
